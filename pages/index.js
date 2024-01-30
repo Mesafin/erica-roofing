9318c51ecb3f451df1afba89a197239ac9dfa1cd
mesafintminwagaw23@gmail.com <mesafintminwagaw23@gmail.com>
2024-01-30 22:36:37 +0300

    whathappens

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,16 @@ import Counter1 from "@/components/sections/Counter1"
 import Testimonial1 from "@/components/sections/Testimonial1"
 import Newsletter1 from "@/components/sections/Newsletter1"
 import AddService from "@/components/sections/AddService"
+import Head from "next/head"
 
 export default function Home() {
 
     return (
+
+
       <>
         <Layout headerCls="transparent-header">
+          <Head/>
           <Banner1 />
           <Features1 />
           <About1 />

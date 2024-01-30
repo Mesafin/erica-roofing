e1fd7818efa1fbc3f6f41452224f3cfc49ccb5f7
mesafintminwagaw23@gmail.com <mesafintminwagaw23@gmail.com>
2024-01-30 22:03:33 +0300

    google add

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -2,14 +2,19 @@ import Head from 'next/head'
 
 const PageHead = ({ headTitle }) => {
     return (
-        <>
-            <Head>
-                <title>
-                    {headTitle ? headTitle : "ERICA - Best roofing services in Canada"}
-                </title>
-            </Head>
-        </>
-    )
+      <>
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="Zl16Yyd260nAEss2Bf_xvuNTv9TPyHGPFvEYGEjpc7k"
+          />
+          <meta name="description" content="best roofing service in canada" />
+          <title>
+            {headTitle ? headTitle : "ERICA - Best roofing services in Canada"}
+          </title>
+        </Head>
+      </>
+    );
 }
 
 export default PageHead

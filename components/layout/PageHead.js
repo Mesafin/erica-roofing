46459676eecd565356858de4e51ca325051066a4
mesafintminwagaw23@gmail.com <mesafintminwagaw23@gmail.com>
2024-01-30 22:18:49 +0300

    verified

--- components/layout/PageHead.js
+++ components/layout/PageHead.js
@@ -4,10 +4,7 @@ const PageHead = ({ headTitle }) => {
     return (
       <>
         <Head>
-          <meta
-            name="google-site-verification"
-            content="Zl16Yyd260nAEss2Bf_xvuNTv9TPyHGPFvEYGEjpc7k"
-          />
+          
           <meta name="description" content="best roofing service in canada" />
           <title>
             {headTitle ? headTitle : "ERICA - Best roofing services in Canada"}
--- pages/_document.js
+++ pages/_document.js
@@ -2,15 +2,23 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="en" id="#top">
-            <Head />
-            <link rel="shortcut icon" href="/assets/img/favicon.png" />
-            <link rel="preconnect" href="https://fonts.bunny.net" />
-            <link href="https://fonts.bunny.net/css?family=dm-sans:400,400i,500,500i,700,700i|poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i" rel="stylesheet" />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    )
+      <Html lang="en" id="#top">
+        <Head>
+          <meta
+            name="google-site-verification"
+            content="Zl16Yyd260nAEss2Bf_xvuNTv9TPyHGPFvEYGEjpc7k"
+          />
+        </Head>
+        <link rel="shortcut icon" href="/assets/img/favicon.png" />
+        <link rel="preconnect" href="https://fonts.bunny.net" />
+        <link
+          href="https://fonts.bunny.net/css?family=dm-sans:400,400i,500,500i,700,700i|poppins:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i"
+          rel="stylesheet"
+        />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
 }
